c9c9a5cfdcd5684e772c7a45aafd2d75ee2253c1
Bre01cc <boliveiraassisreis@gmail.com>
2026-02-03 06:26:38 -0300

    feat:Responsividade em andamento

--- dog-api/src/app/components/header/header.ts
+++ dog-api/src/app/components/header/header.ts
@@ -7,13 +7,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
+//Criando um evento de saida para a informação do filho para o pai
+  @Output() buscaRealizada = new EventEmitter()
 
-  @Output() buscaRealizada = new EventEmitter<string>()
-
+  //Método que vai verificar se o nome é valido para ser exportado
   buscar(nomeRaca: string){
+     console.log(nomeRaca);
+     //Criando uma condicional que vai remover os espaços da raça do cachorro.
     if (nomeRaca.trim()){
+//referenciando o buscaRealizada e usando o metodo emit para dar como saida a raça do cachorro
       this.buscaRealizada.emit(nomeRaca)
     }
   }
+  
 
 }
--- dog-api/src/app/services/dog.ts
+++ dog-api/src/app/services/dog.ts
@@ -1,11 +1,16 @@
+//ferramenta do angular para relizar requisições http
 import { HttpClient } from '@angular/common/http';
+//Essa classe pode ser injetada em outros lugares
 import { Injectable, inject } from '@angular/core';
+//Lida com dados assicronas
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+// Responsavel por realizar a requisição na api dog
 export class Dog {
+  // HttpClient seria o semelhante do fetch do angular 
   private http = inject(HttpClient)
 
   private apiUrl = 'https://dog.ceo/api/breed'
